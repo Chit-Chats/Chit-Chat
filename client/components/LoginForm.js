6d7ce1aef29492f6050890aafa5ca0bbbf57f108
Esther Cho <xesthercho@gmail.com>
2022-06-17 12:56:56 -0700

    added post request to db wih form data

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -2,13 +2,35 @@ import React from 'react';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form'
 import { TextField, Button } from '@mui/material';
+import { Navigate } from "react-router-dom";
 
-
-const LoginForm = (props) => {
+const LoginForm = () => {
   const { register, handleSubmit, formState: { errors }} = useForm();
-  const onSubmit = data => console.log(data);
+  const [loggedInState, setLoggedInState] = useState(false)
 
-  return(
+  //onSubmit should make post request to db
+  const onSubmit = data => {
+    console.log(data)
+    const {username, password} = data
+    fetch('http://localhost:3000/login', {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {username, password},
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setLoggedInState(true)
+    })
+    .catch((err) => {
+      console.log("login error", err);
+      alert('Wrong username/password');
+    });
+  };
+
+  return loggedInState ? <Navigate to="/home" /> : (
     <div id='login-form'>
       <h1>Login</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
