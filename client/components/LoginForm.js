c5326baca0ff3dd187c2d99178a36e7c9c6aab5a
Esther Cho <xesthercho@gmail.com>
2022-06-17 12:22:19 -0700

    styling form with mui

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form'
+import { TextField, Button } from '@mui/material';
+
 
 const LoginForm = (props) => {
+  const { register, handleSubmit, formState: { errors }} = useForm();
+  const onSubmit = data => console.log(data);
+
   return(
-    <div id='login-form'></div>
+    <div id='login-form'>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField 
+          {...register("username", {required: 'Username is required'})} 
+          id='username' 
+          placeholder='username'
+          error={errors?.username ? true : false}
+          helperText={errors?.username ? errors.username.message : null}
+          /> 
+        <br></br>
+        <TextField 
+          {...register("password", {required: 'Password is required'})} 
+          type='password' 
+          id='password' 
+          placeholder='password'
+          error={errors?.password ? true : false}
+          helperText={errors?.password ? errors.password.message : null}
+        />
+        <br></br>
+        <Button 
+          type='submit' 
+          variant='contained' 
+          className="submitbutton"
+        >
+        Submit
+        </Button>
+      </form>
+    </div>
   )
 }
 
