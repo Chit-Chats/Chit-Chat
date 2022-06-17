033111fabfc51d22c154e4705b772b084f1a5f57
Esther Cho <xesthercho@gmail.com>
2022-06-17 10:53:06 -0700

    boilerplate login and signup form components

---
+++ client/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginForm = (props) => {
+  return(
+    <div id='login-form'></div>
+  )
+}
+
+export default LoginForm
---
+++ client/components/SignupForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignupForm = (props) => {
+  return(
+    <div id='signup-form'></div>
+  )
+}
+
+export default SignupForm
