13dfc49c667937136b5726eb47b1c854e6022bf4
Ryan <ryanwallace1396@gmail.com>
2022-06-21 10:49:37 -0700

    created routes to the user endpoint to test request methods (get and post) through postman to validate the path

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -4,10 +4,15 @@ const userController = require('../controllers/userController');
 // create a new router instance
 const userRouter = Router();
 
+userRouter.get('/something', (req, res) => {
+  console.log('in something');
+  res.status(200).send('nice');
+});
+
 userRouter.post('/signup', userController.createUser, (req, res) => {
   console.log('in signup router');
   // respond with a status of 200
-  res.status(200);
+  res.status(200).send('postin');
 });
 
 // export the user router
