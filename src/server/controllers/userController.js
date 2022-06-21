798f38a68d99b70b1c108d93cc9b9c33f50c60d4
Esther Cho <xesthercho@gmail.com>
2022-06-21 11:36:30 -0700

    added additional params to db query

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -9,8 +9,8 @@ userController.createUser = async (req, res, next) => {
   // should receive from the clients post request:
   // first_name, last_name, email, and password
   // use object destructuring to store those properties in variables 
-  const { firstName, lastName, email, password} = req.body;
-  console.log( firstName, lastName, email, password);
+  const { email, username, firstName, lastName, password } = req.body;
+  console.log(email, username, firstName, lastName, password);
   // NEED TO IMPLEMENT PASSWORD HASHING USING 256 ENCRYPTION FOR PASSWORD
   // ====================================================================
 
@@ -21,14 +21,14 @@ userController.createUser = async (req, res, next) => {
 
     const queryString = 
     `
-    INSERT INTO users (first_name, last_name, email, password)
-    VALUES($1, $2, $3, $4)
+    INSERT INTO users (email, username, first_name, last_name, password)
+    VALUES($1, $2, $3, $4, $5)
     RETURNING *;
     `
     // profile_pic and last_logged_in will be added to users table in profile settings
     
     // store the query params in an array to pass into the database query
-    const params = [ firstName, lastName, email, password];
+    const params = [email, username, firstName, lastName, password];
     // query the database, passing in the query string and params and store the result in a variable
     const result = await db.query(queryString, params);
     console.log(result);
